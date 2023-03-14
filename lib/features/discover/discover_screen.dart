8248414f01cc15f52c53176d8666c9fb2f477875
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-03-14 14:34:32 +0900

    feat : create grid view

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,14 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:tictok_clone_flutter/constants/sizes.dart';
+
+final tabs = [
+  "Top",
+  "Users",
+  "Videos",
+  "Sounds",
+  "LIVE",
+  "Shopping",
+  "Brands",
+];
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("discover")),
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          title: const Text("discover"),
+          bottom: TabBar(
+              splashFactory: NoSplash.splashFactory,
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.size16,
+              ),
+              isScrollable: true,
+              indicatorColor: Colors.black,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey.shade500,
+              labelStyle: const TextStyle(
+                  fontWeight: FontWeight.w600, fontSize: Sizes.size16),
+              tabs: [
+                for (var tab in tabs)
+                  Tab(
+                    text: tab,
+                  ),
+              ]),
+        ),
+        body: TabBarView(
+          children: [
+            GridView.builder(
+              padding: const EdgeInsets.all(Sizes.size5),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                mainAxisSpacing: Sizes.size10,
+                crossAxisSpacing: Sizes.size10,
+                crossAxisCount: 2,
+                childAspectRatio: 9 / 16,
+              ),
+              itemBuilder: (context, index) => Container(
+                color: Colors.amber,
+                child: Center(
+                  child: Text('$index'),
+                ),
+              ),
+            ),
+            for (var tab in tabs.skip(1))
+              Center(
+                child: Text(tab),
+              )
+          ],
+        ),
+      ),
     );
   }
 }
