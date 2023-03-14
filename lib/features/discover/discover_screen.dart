3f04e4e0f7da3c38e285d0e3ef3c22ba586c2fff
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-03-14 16:49:16 +0900

    design : add image and text

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tictok_clone_flutter/constants/gaps.dart';
 import 'package:tictok_clone_flutter/constants/sizes.dart';
 
 final tabs = [
@@ -48,13 +50,57 @@ class DiscoverScreen extends StatelessWidget {
                 mainAxisSpacing: Sizes.size10,
                 crossAxisSpacing: Sizes.size10,
                 crossAxisCount: 2,
-                childAspectRatio: 9 / 16,
+                childAspectRatio: 9 / 21,
               ),
-              itemBuilder: (context, index) => Container(
-                color: Colors.amber,
-                child: Center(
-                  child: Text('$index'),
-                ),
+              itemBuilder: (context, index) => Column(
+                children: [
+                  AspectRatio(
+                    aspectRatio: 9 / 16,
+                    child: FadeInImage.assetNetwork(
+                        fit: BoxFit.cover,
+                        placeholder: "assets/images/image1.jpg",
+                        image:
+                            "https://source.unsplash.com/random/200x${355 + index}"),
+                  ),
+                  Gaps.v10,
+                  const Text(
+                    'I\'ve been reading books of oldThe legends and the myths Achilles and his gold Hercules and his gifts',
+                    style: TextStyle(
+                        fontSize: Sizes.size16, fontWeight: FontWeight.bold),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Gaps.v5,
+                  DefaultTextStyle(
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    child: Row(
+                      children: [
+                        const CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              "https://avatars.githubusercontent.com/u/62362753?v=4"),
+                        ),
+                        const Expanded(
+                          child: Text(
+                            'My nickname is very very long ',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        Gaps.h4,
+                        FaIcon(
+                          FontAwesomeIcons.heart,
+                          size: Sizes.size14,
+                          color: Colors.grey.shade600,
+                        ),
+                        Gaps.h2,
+                        const Text('1.5K')
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
             for (var tab in tabs.skip(1))
