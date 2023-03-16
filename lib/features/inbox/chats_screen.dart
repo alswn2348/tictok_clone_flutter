24d23c79b4a0b6fa211788a665e8c79ec32aa1f6
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-03-16 14:07:41 +0900

    feat : delete list item

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tictok_clone_flutter/constants/sizes.dart';
+import 'package:tictok_clone_flutter/features/inbox/chat_detail_screen.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
@@ -14,11 +15,72 @@ class _ChatsScreenState extends State<ChatsScreen> {
 
   final List<int> _items = [];
 
+  final Duration _duration = const Duration(milliseconds: 300);
+
   void _addItem() {
     if (_key.currentState != null) {
-      _key.currentState!.insertItem(_items.length);
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: _duration,
+      );
       _items.add(_items.length);
     }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+        index,
+        (context, animation) => SizeTransition(
+          sizeFactor: animation,
+          child: Container(
+            color: Colors.red,
+            child: _makeTile(index),
+          ),
+        ),
+        duration: _duration,
+      );
+      _items.removeAt(index);
+    }
+  }
+
+  ListTile _makeTile(int index) {
+    return ListTile(
+      onTap: _onChatTap,
+      onLongPress: () => _deleteItem(index),
+      leading: const CircleAvatar(
+        radius: 30,
+        foregroundImage: NetworkImage(
+            "https://avatars.githubusercontent.com/u/62362753?v=4"),
+        child: Text('민추'),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            '$index',
+            style: const TextStyle(fontWeight: FontWeight.w600),
+          ),
+          Text(
+            "3:42PM",
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size14,
+            ),
+          ),
+        ],
+      ),
+      subtitle: const Text('Don\' forger to make video'),
+    );
+  }
+
+  void _onChatTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ChatDetailScreen(),
+      ),
+    );
   }
 
   @override
@@ -27,7 +89,9 @@ class _ChatsScreenState extends State<ChatsScreen> {
         appBar: AppBar(
           actions: [
             IconButton(
-                onPressed: _addItem, icon: const FaIcon(FontAwesomeIcons.plus))
+              onPressed: _addItem,
+              icon: const FaIcon(FontAwesomeIcons.plus),
+            ),
           ],
           elevation: 1,
           title: const Text('Direct messages'),
@@ -41,32 +105,7 @@ class _ChatsScreenState extends State<ChatsScreen> {
               opacity: animation,
               child: SizeTransition(
                 sizeFactor: animation,
-                child: ListTile(
-                  leading: const CircleAvatar(
-                    radius: 30,
-                    foregroundImage: NetworkImage(
-                        "https://avatars.githubusercontent.com/u/62362753?v=4"),
-                    child: Text('민추'),
-                  ),
-                  title: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        '$index',
-                        style: const TextStyle(fontWeight: FontWeight.w600),
-                      ),
-                      Text(
-                        "3:42PM",
-                        style: TextStyle(
-                          color: Colors.grey.shade500,
-                          fontSize: Sizes.size14,
-                        ),
-                      ),
-                    ],
-                  ),
-                  subtitle: const Text('Don\' forger to make video'),
-                ),
+                child: _makeTile(index),
               ),
             );
           },
