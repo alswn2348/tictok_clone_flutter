1ac83c45c14a8e68a3e96a6f10979073c7c6bced
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-03-17 16:49:14 +0900

    feat : add sliver fixed list

--- lib/features/user/user_profile_screen.dart
+++ lib/features/user/user_profile_screen.dart
@@ -15,7 +15,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       slivers: [
         SliverAppBar(
           floating: true,
-          stretch: true,
+          snap: true,
           pinned: true,
           backgroundColor: Colors.teal,
           collapsedHeight: 80,
@@ -31,6 +31,18 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             ),
             title: const Text("Hello!"),
           ),
+        ),
+        SliverFixedExtentList(
+          delegate: SliverChildBuilderDelegate(
+              childCount: 50,
+              (context, index) => Container(
+                    color: Colors.amber[100 * (index % 9)],
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text('index $index'),
+                    ),
+                  )),
+          itemExtent: 100,
         )
       ],
     );
