2a2c614faf7f3da255567a98384b7ee79ae309fd
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-03-20 13:45:58 +0900

    feat : add showDateWidgets
    chage Theme

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -12,6 +12,32 @@ class SettingsScreen extends StatelessWidget {
       body: ListView(
         children: [
           ListTile(
+            onTap: () async {
+              await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1900),
+                lastDate: DateTime(2030),
+              );
+              await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                    data: ThemeData(
+                        appBarTheme: const AppBarTheme(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.black,
+                    )),
+                    child: child!,
+                  );
+                },
+              );
+            },
+            title: const Text('What is your birthday'),
+          ),
+          ListTile(
             onTap: () => showAboutDialog(context: context),
             title: const Text(
               "About",
