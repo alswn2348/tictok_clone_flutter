5daba2aa9eeda838d20237f1cfcadeb6bd64c8f2
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-03-26 20:14:20 +0900

    feat : get permission

---
+++ lib/features/videos/video_recording_screen.dart
@@ -0,0 +1,50 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class VideoRecordingScreen extends StatefulWidget {
+  const VideoRecordingScreen({super.key});
+
+  @override
+  State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
+}
+
+class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+  bool _hasPermission = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initPermissions();
+  }
+
+  Future<void> initPermissions() async {
+    final cameraPermission = await Permission.camera.request();
+    final micPermission = await Permission.microphone.request();
+
+    final cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+
+    final micDenied =
+        micPermission.isDenied || micPermission.isPermanentlyDenied;
+
+    if (!cameraDenied && !micDenied) {
+      _hasPermission = true;
+      await initCamera();
+      setState(() {});
+    }
+  }
+
+  Future<void> initCamera() async {
+    final camera = await availableCameras();
+    print(camera);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(),
+    );
+  }
+}
--- lib/router.dart
+++ lib/router.dart
@@ -1,64 +1,9 @@
 import 'package:go_router/go_router.dart';
-import 'package:tictok_clone_flutter/features/authentication/email_screen.dart';
-import 'package:tictok_clone_flutter/features/authentication/sign_up_screen.dart';
-import 'package:tictok_clone_flutter/features/authentication/username_screen.dart';
+import 'package:tictok_clone_flutter/features/videos/video_recording_screen.dart';
 
-final router = GoRouter(
-  routes: [
-    GoRoute(
-        name: SignUpScreen.routeName,
-        path: SignUpScreen.routeURL,
-        builder: (context, state) => const SignUpScreen(),
-        routes: [
-          GoRoute(
-              path: UsernameScreen.routeURL,
-              name: UsernameScreen.routeName,
-              builder: (context, state) => const UsernameScreen(),
-              routes: [
-                GoRoute(
-                  path: EmailScreen.routeURL,
-                  name: EmailScreen.routeName,
-                  builder: (context, state) {
-                    final args = state.extra as EmailScreenArgs;
-                    return EmailScreen(username: args.userName);
-                  },
-                ),
-              ]),
-        ]),
-    /*    GoRoute(
-      path: LoginScreen.routeName,
-      builder: (context, state) => const LoginScreen(),
-    ), */
-    /* GoRoute(
-      name: "username_screen",
-      path: UsernameScreen.routeName,
-      pageBuilder: (context, state) {
-        return CustomTransitionPage(
-          child: const UsernameScreen(),
-          transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            return FadeTransition(
-              opacity: animation,
-              child: ScaleTransition(
-                scale: animation,
-                child: child,
-              ),
-            );
-          },
-        );
-      },
-    ), */
-/* 
-    GoRoute(
-      path: "/users/:username",
-      builder: (context, state) {
-        final username = state.params['username'];
-        final tab = state.queryParams['show'];
-
-        if (tab != null) {
-          return UserProfileScreen(username: username!, tab: tab);
-        }
-        return UserProfileScreen(username: username!);
-      },
-    ), */
-  ],
-);
+final router = GoRouter(routes: [
+  GoRoute(
+    path: "/",
+    builder: (context, state) => const VideoRecordingScreen(),
+  )
+]);
