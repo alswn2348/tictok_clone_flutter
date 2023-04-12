f3bf375bd7da6fb5342f480c126f3958272ad031
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-04-12 20:40:34 +0900

    feat : Edit saved video
     비디오정보가 firestore 에 저장되면 firebase functions 을 이용해 hello 필드 를 추가

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,14 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
+admin.initializeApp();
+
+export const onVideoCreated = functions.firestore
+  .document("videos/{videoId}")
+  .onCreate(async (snapshot, context) => {
+    await snapshot.ref.update({ hello: "from functions" });
+  });
+  
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
